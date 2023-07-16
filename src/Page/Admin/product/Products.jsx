4489afa2cc36184572e1c9bd7ff9674e8a6d7209
unknown <dunghtph18929@fpt.edu.vn>
2023-07-16 15:56:46 +0700

    crud products

--- src/Page/Admin/product/Products.jsx
+++ src/Page/Admin/product/Products.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from 'react';
+import { Space, Table, Tag,Popconfirm } from 'antd';
+import { Link } from "react-router-dom";
+import { DeleteTwoTone, EditTwoTone, FileAddTwoTone, SearchOutlined } from '@ant-design/icons';
 
-const Products = () => {
-    return (
-        <div>Products</div>
-    )
-}
-
-export default Products
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+          <Link><EditTwoTone style={{ fontSize: '20px', color: '#08c' }} /></Link>
+          <Popconfirm
+            title="Delete the task"
+            description="Are you sure to delete this task?"
+            // onConfirm={() => onHandleDelete(record.id)}
+            okText="Yes"
+            cancelText="No"
+            okButtonProps={{ className: "text-light bg-primary" }}
+          >
+            <DeleteTwoTone style={{ fontSize: '18px' }} />
+          </Popconfirm>      </Space>
+    ),
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+const products = () => <Table columns={columns} dataSource={data} />;
+export default products;
