b709aa83d22f54af29b0b9b11666b3cbfd0314e3
unknown <dunghtph18929@fpt.edu.vn>
2023-07-18 00:37:31 +0700

    product fontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPage from './Page/Admin/HomePage/AdminPage';
 import Products from './Page/Admin/product/Products';
-// import AddProducts from './Page/Admin/product/AddProducts';
+import AddProducts from './Page/Admin/product/AddProducts';
 import AdminLayout from './Layout/AdminLayout';
 import Categories from './Page/Admin/Category/Categories';
 import AddCategory from './Page/Admin/Category/AddCategory';
@@ -22,7 +22,7 @@ function App() {
           <Route path="category" element={<Categories />} />
           <Route path="category/add" element={<AddCategory />} />
           <Route path="products" element={<Products />} />
-          {/* <Route path="products/add" element={<AddProducts />} /> */}
+          <Route path="products/add" element={<AddProducts />} />
 
     
         </Route>
--- src/Page/Admin/product/Products.jsx
+++ src/Page/Admin/product/Products.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import productApi from '../../../api/products';
+import categoryApi from '../../../api/category';
 import { Space, Table, Popconfirm, Button } from 'antd';
 import { DeleteTwoTone, EditTwoTone, FileAddTwoTone } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const { Column } = Table;
 
-const Products = () => {
+const Products = ({ categoryList }) => {
   const [productList, setProductList] = useState([]);
 
   useEffect(() => {
@@ -16,22 +17,22 @@ const Products = () => {
         console.log('response', response.docs);
         setProductList(response.docs);
       } catch (error) {
-        console.log('Failed to fetch CategoryList', error);
+        console.log('Failed to fetch ProductList', error);
       }
     };
     fetchProductList();
   }, []);
 
   const handleDelete = async (id) => {
-    console.log( "_id", id);
+    console.log("_id", id);
     try {
       const response = await productApi.Remove(id);
-        if (response.status === 200) {
-          console.log("Xóa thành công");
-          useEffect();
-        } else {
-          console.log('Xóa không thành công');
-        }
+      if (response.status === 200) {
+        console.log("Xóa thành công");
+        useEffect();
+      } else {
+        console.log('Xóa không thành công');
+      }
     } catch (error) {
       console.log('Failed to delete product', error);
     }
@@ -40,16 +41,25 @@ const Products = () => {
   return (
     <div>
       <div style={{ marginBottom: 16, display: 'flex', justifyContent: 'flex-end' }}>
-       <Link><Button type="primary" icon={<FileAddTwoTone />}>
-          Add New
-        </Button></Link> 
+        <Link to={'/admin/products/add'}>
+          <Button type="primary" icon={<FileAddTwoTone />}>
+            Add New
+          </Button>
+        </Link>
       </div>
       <Table dataSource={productList}>
-      {/* <Column title="id" dataIndex="dât" key="_id" /> */}
-
+        {/* <Column title="id" dataIndex="dât" key="_id" /> */}
         <Column title="Name" dataIndex="name" key="name" />
         <Column title="Description" dataIndex="description" key="description" />
-
+        <Column
+          title="Category"
+          dataIndex="categoryId"
+          key="categoryId"
+          render={(categoryId) => {
+            const category = categoryList.find((item) => item._id === categoryId);
+            return category ? category.name : '';
+          }}
+        />
         <Column
           title="Action"
           render={(record) => (
@@ -75,4 +85,23 @@ const Products = () => {
   );
 };
 
-export default Products;
+const ProductsContainer = () => {
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    const fetchCategoryList = async () => {
+      try {
+        const response = await categoryApi.GetAll();
+        console.log('response', response);
+        setCategoryList(response);
+      } catch (error) {
+        console.log('Failed to fetch CategoryList', error);
+      }
+    };
+    fetchCategoryList();
+  }, []);
+
+  return <Products categoryList={categoryList} />;
+};
+
+export default ProductsContainer;
