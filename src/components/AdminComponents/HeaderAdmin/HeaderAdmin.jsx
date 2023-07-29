c4fb2a0acc23249bc546f8044be73cc6eaed45d6
truonghvph16694 <96322678+truonghvph16694@users.noreply.github.com>
2023-07-29 14:29:32 +0700

    update

--- src/components/AdminComponents/HeaderAdmin/HeaderAdmin.jsx
+++ src/components/AdminComponents/HeaderAdmin/HeaderAdmin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, message } from 'antd';
 import { Header } from 'antd/es/layout/layout';
 import SubMenu from 'antd/es/menu/SubMenu';
 import { UserOutlined, LaptopOutlined, NotificationOutlined, LogoutOutlined } from '@ant-design/icons';
